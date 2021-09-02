febef2d80d7935db43c182de03ca35096f3d1328
arydr <dsenicero00@gmail.com>
2021-09-02 15:31:54 -0500

    Changed outline

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace BCD_Restaurant_Project
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(416, 188);
+            this.button1.Location = new System.Drawing.Point(204, 215);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -46,7 +46,7 @@ namespace BCD_Restaurant_Project
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(482, 453);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
