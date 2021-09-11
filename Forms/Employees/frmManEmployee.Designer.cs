6e00f926d7bb590a016b42c3200a8f8ba6147833
brayan599 <baviles599@gmail.com>
2021-09-11 12:24:33 -0500

    Employee form

---
+++ Forms/Employees/frmManEmployee.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace BCD_Restaurant_Project.Forms
+{
+    partial class frmManEmployee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmManEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 415);
+            this.Name = "frmManEmployee";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manage Employees";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
