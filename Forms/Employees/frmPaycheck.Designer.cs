0c2861e3a3b240c0be3f4541607c1b06214b6478
arydr <dsenicero00@gmail.com>
2021-09-16 10:37:32 -0500

    added the paycheck form

---
+++ Forms/Employees/frmPaycheck.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace BCD_Restaurant_Project.Forms.Employees
+{
+    partial class frmPaycheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNetPay = new System.Windows.Forms.Label();
+            this.lblOfNetPay = new System.Windows.Forms.Label();
+            this.lblFICAWithheld = new System.Windows.Forms.Label();
+            this.lblOfFicaWithheld = new System.Windows.Forms.Label();
+            this.lblSocialSecurityWithheld = new System.Windows.Forms.Label();
+            this.lblOfSsWithheld = new System.Windows.Forms.Label();
+            this.lblGrossPay = new System.Windows.Forms.Label();
+            this.lblOfGrossPay = new System.Windows.Forms.Label();
+            this.lblHoursWorked = new System.Windows.Forms.Label();
+            this.lblOfWklyHrs = new System.Windows.Forms.Label();
+            this.lblHourlyRate = new System.Windows.Forms.Label();
+            this.lblOfHrlyRt = new System.Windows.Forms.Label();
+            this.lblEmployeeName = new System.Windows.Forms.Label();
+            this.lblOfEmpName = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblEmployeeID = new System.Windows.Forms.Label();
+            this.lblOfEmployeeId = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNetPay
+            // 
+            this.lblNetPay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNetPay.Location = new System.Drawing.Point(271, 402);
+            this.lblNetPay.Name = "lblNetPay";
+            this.lblNetPay.Size = new System.Drawing.Size(148, 26);
+            this.lblNetPay.TabIndex = 20;
+            this.lblNetPay.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOfNetPay
+            // 
+            this.lblOfNetPay.Location = new System.Drawing.Point(40, 402);
+            this.lblOfNetPay.Name = "lblOfNetPay";
+            this.lblOfNetPay.Size = new System.Drawing.Size(206, 26);
+            this.lblOfNetPay.TabIndex = 19;
+            this.lblOfNetPay.Text = "Net Pay:";
+            this.lblOfNetPay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblFICAWithheld
+            // 
+            this.lblFICAWithheld.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFICAWithheld.Location = new System.Drawing.Point(271, 368);
+            this.lblFICAWithheld.Name = "lblFICAWithheld";
+            this.lblFICAWithheld.Size = new System.Drawing.Size(148, 26);
+            this.lblFICAWithheld.TabIndex = 18;
+            this.lblFICAWithheld.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOfFicaWithheld
+            // 
+            this.lblOfFicaWithheld.Location = new System.Drawing.Point(40, 368);
+            this.lblOfFicaWithheld.Name = "lblOfFicaWithheld";
+            this.lblOfFicaWithheld.Size = new System.Drawing.Size(206, 26);
+            this.lblOfFicaWithheld.TabIndex = 17;
+            this.lblOfFicaWithheld.Text = "FICA Withheld:";
+            this.lblOfFicaWithheld.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblSocialSecurityWithheld
+            // 
+            this.lblSocialSecurityWithheld.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSocialSecurityWithheld.Location = new System.Drawing.Point(271, 334);
+            this.lblSocialSecurityWithheld.Name = "lblSocialSecurityWithheld";
+            this.lblSocialSecurityWithheld.Size = new System.Drawing.Size(148, 26);
+            this.lblSocialSecurityWithheld.TabIndex = 16;
+            this.lblSocialSecurityWithheld.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOfSsWithheld
+            // 
+            this.lblOfSsWithheld.Location = new System.Drawing.Point(40, 334);
+            this.lblOfSsWithheld.Name = "lblOfSsWithheld";
+            this.lblOfSsWithheld.Size = new System.Drawing.Size(206, 26);
+            this.lblOfSsWithheld.TabIndex = 15;
+            this.lblOfSsWithheld.Text = "Retirement Plan:";
+            this.lblOfSsWithheld.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblGrossPay
+            // 
+            this.lblGrossPay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGrossPay.Location = new System.Drawing.Point(271, 300);
+            this.lblGrossPay.Name = "lblGrossPay";
+            this.lblGrossPay.Size = new System.Drawing.Size(148, 26);
+            this.lblGrossPay.TabIndex = 14;
+            this.lblGrossPay.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOfGrossPay
+            // 
+            this.lblOfGrossPay.Location = new System.Drawing.Point(59, 300);
+            this.lblOfGrossPay.Name = "lblOfGrossPay";
+            this.lblOfGrossPay.Size = new System.Drawing.Size(206, 26);
+            this.lblOfGrossPay.TabIndex = 13;
+            this.lblOfGrossPay.Text = "Gross Pay:";
+            this.lblOfGrossPay.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblHoursWorked
+            // 
+            this.lblHoursWorked.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHoursWorked.Location = new System.Drawing.Point(271, 219);
+            this.lblHoursWorked.Name = "lblHoursWorked";
+            this.lblHoursWorked.Size = new System.Drawing.Size(148, 26);
+            this.lblHoursWorked.TabIndex = 8;
+            this.lblHoursWorked.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOfWklyHrs
+            // 
+            this.lblOfWklyHrs.Location = new System.Drawing.Point(40, 219);
+            this.lblOfWklyHrs.Name = "lblOfWklyHrs";
+            this.lblOfWklyHrs.Size = new System.Drawing.Size(206, 26);
+            this.lblOfWklyHrs.TabIndex = 7;
+            this.lblOfWklyHrs.Text = "Weekly Hours Worked:";
+            this.lblOfWklyHrs.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblHourlyRate
+            // 
+            this.lblHourlyRate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHourlyRate.Location = new System.Drawing.Point(271, 142);
+            this.lblHourlyRate.Name = "lblHourlyRate";
+            this.lblHourlyRate.Size = new System.Drawing.Size(148, 26);
+            this.lblHourlyRate.TabIndex = 6;
+            this.lblHourlyRate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOfHrlyRt
+            // 
+            this.lblOfHrlyRt.Location = new System.Drawing.Point(40, 142);
+            this.lblOfHrlyRt.Name = "lblOfHrlyRt";
+            this.lblOfHrlyRt.Size = new System.Drawing.Size(206, 26);
+            this.lblOfHrlyRt.TabIndex = 5;
+            this.lblOfHrlyRt.Text = "Hourly Rate:";
+            this.lblOfHrlyRt.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblEmployeeName
+            // 
+            this.lblEmployeeName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEmployeeName.Location = new System.Drawing.Point(271, 108);
+            this.lblEmployeeName.Name = "lblEmployeeName";
+            this.lblEmployeeName.Size = new System.Drawing.Size(208, 26);
+            this.lblEmployeeName.TabIndex = 4;
+            this.lblEmployeeName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOfEmpName
+            // 
+            this.lblOfEmpName.Location = new System.Drawing.Point(40, 108);
+            this.lblOfEmpName.Name = "lblOfEmpName";
+            this.lblOfEmpName.Size = new System.Drawing.Size(206, 26);
+            this.lblOfEmpName.TabIndex = 3;
+            this.lblOfEmpName.Text = "Employee Name:";
+            this.lblOfEmpName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(194, 35);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(150, 26);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "PAY CHECK";
+            // 
+            // lblEmployeeID
+            // 
+            this.lblEmployeeID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEmployeeID.Location = new System.Drawing.Point(271, 74);
+            this.lblEmployeeID.Name = "lblEmployeeID";
+            this.lblEmployeeID.Size = new System.Drawing.Size(148, 26);
+            this.lblEmployeeID.TabIndex = 2;
+            this.lblEmployeeID.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOfEmployeeId
+            // 
+            this.lblOfEmployeeId.Location = new System.Drawing.Point(40, 74);
+            this.lblOfEmployeeId.Name = "lblOfEmployeeId";
+            this.lblOfEmployeeId.Size = new System.Drawing.Size(206, 26);
+            this.lblOfEmployeeId.TabIndex = 1;
+            this.lblOfEmployeeId.Text = "Employee ID:";
+            this.lblOfEmployeeId.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(271, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 26);
+            this.label1.TabIndex = 22;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(40, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 26);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Tips Received:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(271, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 26);
+            this.label3.TabIndex = 24;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(40, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 26);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Salaried:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // frmPaycheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 471);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNetPay);
+            this.Controls.Add(this.lblOfNetPay);
+            this.Controls.Add(this.lblFICAWithheld);
+            this.Controls.Add(this.lblOfFicaWithheld);
+            this.Controls.Add(this.lblSocialSecurityWithheld);
+            this.Controls.Add(this.lblOfSsWithheld);
+            this.Controls.Add(this.lblGrossPay);
+            this.Controls.Add(this.lblOfGrossPay);
+            this.Controls.Add(this.lblHoursWorked);
+            this.Controls.Add(this.lblOfWklyHrs);
+            this.Controls.Add(this.lblHourlyRate);
+            this.Controls.Add(this.lblOfHrlyRt);
+            this.Controls.Add(this.lblEmployeeName);
+            this.Controls.Add(this.lblOfEmpName);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblEmployeeID);
+            this.Controls.Add(this.lblOfEmployeeId);
+            this.Name = "frmPaycheck";
+            this.Text = "frmPaycheck";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblNetPay;
+        internal System.Windows.Forms.Label lblOfNetPay;
+        internal System.Windows.Forms.Label lblFICAWithheld;
+        internal System.Windows.Forms.Label lblOfFicaWithheld;
+        internal System.Windows.Forms.Label lblSocialSecurityWithheld;
+        internal System.Windows.Forms.Label lblOfSsWithheld;
+        internal System.Windows.Forms.Label lblGrossPay;
+        internal System.Windows.Forms.Label lblOfGrossPay;
+        internal System.Windows.Forms.Label lblHoursWorked;
+        internal System.Windows.Forms.Label lblOfWklyHrs;
+        internal System.Windows.Forms.Label lblHourlyRate;
+        internal System.Windows.Forms.Label lblOfHrlyRt;
+        internal System.Windows.Forms.Label lblEmployeeName;
+        internal System.Windows.Forms.Label lblOfEmpName;
+        internal System.Windows.Forms.Label lblTitle;
+        internal System.Windows.Forms.Label lblEmployeeID;
+        internal System.Windows.Forms.Label lblOfEmployeeId;
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.Label label3;
+        internal System.Windows.Forms.Label label4;
+    }
+}
