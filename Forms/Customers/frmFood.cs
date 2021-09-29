15364b137018f3ffcf1938725e5d025e6295bf1c
brayan599 <baviles599@gmail.com>
2021-09-28 22:21:59 -0500

    Datagridview

--- Forms/Customers/frmFood.cs
+++ Forms/Customers/frmFood.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using BCD_Restaurant_Project.Classes;
 namespace BCD_Restaurant_Project.Forms
 {
     public partial class frmFood : Form
@@ -19,7 +19,9 @@ namespace BCD_Restaurant_Project.Forms
 
         private void frmFood_Load(object sender, EventArgs e)
         {
-
+            ProgOps.DisplayMenuItems(dataGridView1);
         }
+
+        
     }
 }
