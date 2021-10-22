eb8256eaedb04ec7b63c00452e43f602d0f42eed
arydr <dsenicero00@gmail.com>
2021-10-21 23:07:12 -0500

    polished cart class some

--- Classes/MenuItem.cs
+++ Classes/MenuItem.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace BCD_Restaurant_Project.Classes
+﻿namespace BCD_Restaurant_Project.Classes
 {
     class MenuItem
     {
