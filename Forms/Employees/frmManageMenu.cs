14c4e7b91a4d05464c4b4f3a05e0f101853aa5a8
arydr <dsenicero00@gmail.com>
2021-10-25 15:08:12 -0500

    s

--- Forms/Employees/frmManageMenu.cs
+++ Forms/Employees/frmManageMenu.cs
@@ -16,7 +16,7 @@ namespace BCD_Restaurant_Project.Forms.Employees
         private string myState;
         private void frmManageMenu_Load(object sender, EventArgs e)
         {
-            ProgOps.modifyMenu(tbxName, tbxItemID, tbxDescription, tbxPrice, tbxImagePath, cbxCategories, this, out menuManager);
+            ProgOps.modifyMenu(tbxName, tbxItemID, tbxDescription, tbxPrice, tbxImagePath, cbxCategories, this, out menuManager, dgvDisplay);
 
 
             //byte[] image = System.Text.Encoding.UTF8.GetBytes(tbxImagePath.Text);
