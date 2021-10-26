46584c7b4155dc89e4408dbb93ca83e68b567505
Brayan <baviles599@gmail.com>
2021-10-26 10:34:26 -0500

    Payment

--- Forms/Customers/frmPayment.Designer.cs
+++ Forms/Customers/frmPayment.Designer.cs
@@ -203,6 +203,7 @@ namespace BCD_Restaurant_Project.Forms.Customers
             this.btnCancel.TabIndex = 42;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // frmPayment
             // 
